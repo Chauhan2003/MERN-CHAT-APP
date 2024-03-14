450a244dd3a85a146c6bfad9677f0c58f2191070
gagan-chauhan <gagan2020chauhan@gmail.com>
2024-03-15 00:45:21 +0530

    Error Handling

--- server/index.js
+++ server/index.js
@@ -16,3 +16,14 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`)
 })
+
+// Error Handling:
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+    return res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message,
+    });
+})
