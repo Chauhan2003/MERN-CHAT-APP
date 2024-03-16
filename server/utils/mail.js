ac2fc1c9930f600eb08f4d0479c61ba108d76ea1
gagan-chauhan <gagan2020chauhan@gmail.com>
2024-03-16 10:15:44 +0530

    changed the style of mail

--- server/utils/mail.js
+++ server/utils/mail.js
@@ -15,16 +15,17 @@ export const sendPasswordMail = async (id, token, email_to) => {
 
     var resetPasswordMsg = `
     <div style="background-color: #f8f8f8; padding: 20px; border-radius: 10px; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);">
-        <h1 style="color: #5bc0de; text-align: center; font-family: 'Helvetica Neue', sans-serif;">Reset Your Password</h1>
-        <p style="color: #333; font-size: 16px; text-align: center; line-height: 1.6;">
-            We received a request to reset your password. Please click the link below to proceed.
-        </p>
-        <div style="text-align: center; margin-top: 20px;">
-            <a href="http://localhost:3000/resetpassword/${id}/${token}" style="display: inline-block; padding: 12px 24px; background-color: #5bc0de; color: #fff; text-decoration: none; font-size: 18px; border-radius: 5px; cursor: pointer;">
-                Reset Password
-            </a>
-        </div>
+    <h1 style="color: #6159CB; text-align: center; font-family: 'Helvetica Neue', sans-serif;">Reset Your Password</h1>
+    <p style="color: #333; font-size: 16px; text-align: center; line-height: 1.6;">
+        We received a request to reset your password. Please click the link below to proceed.
+    </p>
+    <div style="text-align: center; margin-top: 20px;">
+        <a href="http://localhost:3000/resetpassword/${id}/${token}" style="display: inline-block; padding: 12px 24px; background-color: #6159CB; color: #fff; text-decoration: none; font-size: 18px; border-radius: 5px; cursor: pointer;">
+            Reset Password
+        </a>
     </div>
+</div>
+
 `;
 
 
